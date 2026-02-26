c00ea7b27c8edafe6e67cb42f8048065065ad0e6
Riq <ruylivinginadream@duck.com>
2026-02-26 10:39:53 -0300

    AppPerfil

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { View, Text, StyleSheet, SafeAreaView, ScrollView } from 'react-native';
 import Cabecalho from './src/components/Cabecalho';
 import Rodape from './src/components/Rodape';
 import Perfil from './src/components/Perfil';
+import CardUsuario from './src/components/CardUsuario';
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Cabecalho mensagem="Meu app de perfil" />
       <ScrollView>
         <Perfil nome="eu" email = "eu@email.com" idade={30} cidade="São Paulo" />
+        <CardUsuario nome="Maria" email="maria@email.com" />
       </ScrollView>
       <Rodape mensagem="© 2025 - Meu App" />
     </View>
